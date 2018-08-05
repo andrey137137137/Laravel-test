e1033307e6b4ce878d4f01e1e0d72cd10423fe5f
Andrey Bannikov <andrey27x777@gmail.com>
2018-08-06 02:15:41 +0300

    Corrected interval between mail sending

--- app/Http/Controllers/Application/UserController.php
+++ app/Http/Controllers/Application/UserController.php
@@ -31,8 +31,7 @@ class UserController extends Controller
    *
    * @var integer
    */
-  // private $intervalBetweenSending = 1440;
-  private $intervalBetweenSending = 0;
+  private $intervalBetweenSending = 1440;
 
   /**
    * Вызывает родительский конструктор
